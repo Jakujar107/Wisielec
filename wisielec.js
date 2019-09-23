40452892fa6f729787675426706049544eca315f
Jakujar107 <32633810+Jakujar107@users.noreply.github.com>
2019-09-23 12:56:31 +0200

    Add files via upload

--- wisielec.js
+++ wisielec.js
@@ -1,45 +1,45 @@
-var yes= new Audio("yes.wav");
-var no= new Audio("no.wav");
+const yes= new Audio("yes.wav");
+const no= new Audio("no.wav");
 
-var password ="frajerem jesteś";
-var count=0;
+let tab =["frajerem jesteś","dupa","machiny oblężnicze","malarstwo jaskiniowe",
+'rachunek różniczkowy','rabuś kokosowy','rachunek prawdopodobieństwa','zaaklimatyzować się','złudzenie geometryczne','zżółknięty',' brzuchomówca'];
+let count=0;
+let w=Math.floor(Math.random()*(tab.length));
+let password=tab[w];
+
 password=password.toUpperCase();
-var length=password.length;
 
-var password1= "";
+let length=password.length;
+
+let password1= "";
 
 for(i=0;i<length;i++){
 if(password.charAt(i)==" ")
 	password1=password1+" ";
+
 else password1=password1+"-";
 }
 
-function password_out()
-
-{
+function password_out(){
 	document.getElementById("plansza").innerHTML= password1;
 }
 
 window.onload=start;
 
-var litery =["A","Ą","B","C","Ć","D","E","Ę","F",
+const litery =["A","Ą","B","C","Ć","D","E","Ę","F",
 	"G","H","I","J","K","L","Ł","M","N","Ń","O","Ó","Q","P"
 	,"R","S","Ś","T","U","V","W","X","Y","Z","Ż","Ź"];
 
-function start()
-{
-	
-	var divtext="";
-	for(i=0;i<=34;i++)
-	{
-				var element="lit"+i;
+function start(){
+	let divtext="";
+	for(i=0;i<=34;i++){
+		let element="lit"+i;
 
 		divtext=divtext+'<div class="litera"onclick="check('+i+')" id="'+element+'">'+litery[i]+'</div>';
 		if((i+1)%7===0)divtext=divtext+'<div style="clear:both;"></div>';
 	}
+	
 	document.getElementById("alpha").innerHTML=divtext;
-	
-	
 	password_out();
 }
 
@@ -50,10 +50,8 @@ return this.substr(0,place)+znak+this.substr(place+1);
 }
 }
 
-function check(nr)
-{
-	
-	var gool=false;
+function check(nr){
+	let gool=false;
 	
 	for(i=0;i<length;i++){
 		if(password.charAt(i)==litery[nr])
@@ -62,7 +60,7 @@ function check(nr)
 			gool=true;
 		}
 	}
-			var element="lit"+nr;
+			let element="lit"+nr;
 
 	if(gool==true){
 		yes.play();
@@ -71,25 +69,23 @@ function check(nr)
 		document.getElementById(element).style.border = "3px solid #00C000";
 		document.getElementById(element).style.cursor = "default";
 
-			
 			password_out();
 
 	}
 	else{
-no.play();
+		no.play();
 		document.getElementById(element).style.background = "#200000";
 		document.getElementById(element).style.color = "#C00000";
 		document.getElementById(element).style.border = "3px solid #C00000";
 		document.getElementById(element).style.cursor = "default";
 	count++;
-	var pic="img/s"+count+".jpg";
+	let pic="img/s"+count+".jpg";
 	document.getElementById("wisielec").innerHTML='<img src="'+pic+'"alt=""/>';
-
 	}
 	
 	//wygrana
 	if(password==password1){
-	document.getElementById("alpha").innerHTML="Prawidłowe hasło: " +password+
+	document.getElementById("alpha").innerHTML="BRAWO, zgadłeś!!"+
 	'<br/><br/><span class="reset" onclick="location.reload()">Jeszcze raz?</span>';
 	}
 	if(count>=9){
